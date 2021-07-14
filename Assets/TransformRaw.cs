04471784acaafc5c9869fee193f443d64ac1f70f
Apouchkal <apouchkal@gmail.com>
2021-07-14 18:22:43 +0200

    move folder and test first idea
    Move DOTween to assets because it don't work otherwise
    Move Exemple Assets to plugins
    Test first idea to use tag to modify food to other food

---
+++ Assets/TransformRaw.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cook
+{
+    public class TransformRaw : MonoBehaviour
+    {
+        [SerializeField]
+        private string actionName;
+        [SerializeField]
+        private List<GameObject> results = new List<GameObject>();
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag(actionName))
+            {
+                InstantiateResult();
+                Destroy(this.gameObject);
+            }
+        }
+
+        private void InstantiateResult()
+        {
+            results.ForEach((result) => Instantiate(result, this.transform.position, Quaternion.identity));
+        }
+    }
+}
